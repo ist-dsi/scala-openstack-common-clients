56d2184e6c08c97642e8ec9719b62406369d20f3
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2020-12-14 16:15:01 +0000

    Better show for Usage.

--- src/main/scala/pt/tecnico/dsi/openstack/common/models/Usage.scala
+++ src/main/scala/pt/tecnico/dsi/openstack/common/models/Usage.scala
@@ -1,11 +1,13 @@
 package pt.tecnico.dsi.openstack.common.models
 
-import cats.{Show, derived}
+import cats.Show
+import cats.implicits.showInterpolator
 import io.circe.Decoder
 import io.circe.derivation.{deriveDecoder, renaming}
 
 object Usage {
   implicit def decoder[T: Decoder]: Decoder[Usage[T]] = deriveDecoder(renaming.snakeCase)
-  implicit def show[T: Show]: Show[Usage[T]] = derived.semiauto.show
+  implicit def show[T: Show]: Show[Usage[T]] = (usage: Usage[T]) =>
+    show"${usage.inUse} of ${usage.limit} in use. ${usage.reserved} are reserved."
 }
-final case class Usage[T](inUse: T, limit: T, reserved: T, allocated: Option[T] = None)
+final case class Usage[T](inUse: T, limit: T, reserved: T)
